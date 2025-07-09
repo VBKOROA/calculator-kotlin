aba84e22f8d4250649477928f79ca7e1f2cf3a30
abko <abko121115@gmail.com>
2025-07-09 14:14:19 +0900

    feat: 가장 오래된 결과 지우기

--- app/src/main/kotlin/org/example/Calculator.kt
+++ app/src/main/kotlin/org/example/Calculator.kt
@@ -1,6 +1,11 @@
 package org.example
 
 class Calculator {
+    // static 선언 영역
+    companion object {
+        const val MAX_RESULTS = 5 // static final int MAX_RESULTS와 유사
+    }
+
     private val results = mutableListOf<Int>()
 
     fun getResults(): List<Int> {
@@ -10,6 +15,12 @@ class Calculator {
     fun setResults(newResults: List<Int>) {
         results.clear() // 먼저 기존 결과를 지우고
         results.addAll(newResults) // 새로운 결과를 추가
+    }
+
+    fun removeOldResult() {
+        if (results.isNotEmpty()) {
+            results.removeAt(0) // 가장 오래된 결과를 제거
+        }
     }
 
     fun calculate(num1: Int, num2: Int, op: String): Int? {
@@ -23,7 +34,12 @@ class Calculator {
 
         // result가 null이 아닐 때만 동작
         // it - 자기 자신
-        result?.let { results.add(it) }
+        result?.let { 
+            results.add(it) 
+            if(results.size > MAX_RESULTS) {
+                removeOldResult() // 결과가 MAX_RESULTS개를 초과하면 가장 오래된 결과를 제거
+            }
+        }
         return result
     }
 }
