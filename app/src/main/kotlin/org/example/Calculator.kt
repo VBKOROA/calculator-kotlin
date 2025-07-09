02f2c2d4f57c5f97c260cc76cb652c95c19428fc
abko <abko121115@gmail.com>
2025-07-09 14:08:09 +0900

    feat: getter와 setter 추가

--- app/src/main/kotlin/org/example/Calculator.kt
+++ app/src/main/kotlin/org/example/Calculator.kt
@@ -3,6 +3,15 @@ package org.example
 class Calculator {
     private val results = mutableListOf<Int>()
 
+    fun getResults(): List<Int> {
+        return results
+    }
+
+    fun setResults(newResults: List<Int>) {
+        results.clear() // 먼저 기존 결과를 지우고
+        results.addAll(newResults) // 새로운 결과를 추가
+    }
+
     fun calculate(num1: Int, num2: Int, op: String): Int? {
         val result = when (op) {
             "+" -> num1 + num2
@@ -11,7 +20,7 @@ class Calculator {
             "/" -> if (num2 != 0) num1 / num2 else null
             else -> null
         }
-        
+
         // result가 null이 아닐 때만 동작
         // it - 자기 자신
         result?.let { results.add(it) }
